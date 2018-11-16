b64376e8d4dead9350e61a7d156a6e9581c762ba
Simone C <connola.simone@gmail.com>
2018-11-16 13:57:07 +0100

    Programmatic view and view controller

---
+++ Programmatic/ProgrammaticView.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+open class ProgrammaticView: UIView {
+    
+    public init() {
+        super.init(frame: .zero)
+    }
+    
+    @available(*, unavailable)
+    public required init?(coder aDecoder: NSCoder) { fatalError() }
+}
---
+++ Programmatic/ProgrammaticViewController.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+open class ProgrammaticViewController: UIViewController {
+    
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    public required init?(coder aDecoder: NSCoder) { fatalError() }
+}
